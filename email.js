6c19a69f2374dcfd9705cfea0f24220b45bfb6b1
jaison-silva <jaisonjoy3032gmail.com>
2023-12-01 01:12:36 +0530

    done

--- email.js
+++ email.js
@@ -1,11 +1,17 @@
 function displayValue() {
-    // Get the entered value from the input element
-    var enteredValue = document.getElementById("inputValue").value;
+  // Get the entered value from the input element
+  var enteredValue = document.getElementById("inputValue").value;
 
-    // Display the entered value in the modal
-    document.getElementById("modalBody").innerHTML = "<p>" + enteredValue + "</p>";
+  // Check if the entered value is a valid email
+  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(enteredValue)) {
+      // Valid email, display in the modal
+      document.getElementById("modalBody").innerHTML = "<p>" + enteredValue + "</p>";
 
-    // Show the modal
-    var valueModal = new bootstrap.Modal(document.getElementById('valueModal'));
-    valueModal.show();
+      // Show the modal
+      var valueModal = new bootstrap.Modal(document.getElementById('valueModal'));
+      valueModal.show();
+  } else {
+      // Invalid email, show an error message or handle as needed
+      alert('Invalid email! Please enter a valid email address.');
   }
+}
